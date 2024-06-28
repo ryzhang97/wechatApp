8e828e1d09d5155dc1e9150c2e712167916da652
zhangruyi <1067852@greatwall.com.cn>
2024-06-28 22:21:44 +0800

    网络请求提取到公共的api中

---
+++ api.js
@@ -0,0 +1,40 @@
+const app=getApp()
+const header = {'content-type': 'application/json'}
+
+// post
+ function post(url, data) {
+  return new Promise(resolve => {
+    wx.request({
+      url: app.ip + url,
+      method: 'POST',
+      header,
+      data,
+      success(res) {
+        if(res.data.code==200){
+          resolve(res.data.data)
+        }else{
+          console.error(res.data)
+        }
+      },fail(err){
+        console.error(err)
+      }
+    })
+  })
+}
+
+// get
+ function get(url, data) {
+  return new Promise(resolve => {
+    wx.request({
+      url: app.ip + url,
+      data,
+      success(res) {
+        resolve(res.data.data)
+      }
+    })
+  })
+}
+module.exports={
+  post,
+  get
+}
