f6e42bd1e683eae8223dd70d23654fd312d306a5
zhangruyi <ryzhang97@qq.com>
2024-06-28 17:57:06 +0800

    网络请求练习

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,33 @@
 // index.js
-Page({})
+Page({
+  data:{
+    login:"",
+    token:""
+  },
+  onShow(){
+    this.setData({login:"login",token:'getToken'})
+  },
+  onLogin(){
+    wx.login({
+      success (res) {
+        if (res.code) {
+          wx.request({
+            url: 'http://127.0.0.1:8000/wx/login',
+            method:'POST',
+            data: {
+              code: res.code
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+  },
+  onGetToken(){
+    wx.request({
+      url: 'http://127.0.0.1:8000/wx/getAccess',
+      data: {}
+    })
+  }
+})
