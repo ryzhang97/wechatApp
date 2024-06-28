381b942a1261c7c7c6bce63ff6f66e61181b6546
zhangruyi <1067852@greatwall.com.cn>
2024-06-28 22:24:18 +0800

    获取OpenId

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,33 +1,24 @@
 // index.js
+import {getOpenid} from '../../pub'
 Page({
-  data:{
-    login:"",
-    token:""
+  data: {
+    login: "",
+    token: "",
+    openid:""
   },
-  onShow(){
-    this.setData({login:"login",token:'getToken'})
-  },
-  onLogin(){
-    wx.login({
-      success (res) {
-        if (res.code) {
-          wx.request({
-            url: 'http://127.0.0.1:8000/wx/login',
-            method:'POST',
-            data: {
-              code: res.code
-            }
-          })
-        } else {
-          console.log('登录失败！' + res.errMsg)
-        }
-      }
+  onShow() {
+    this.setData({
+      login: "getOpenid",
+      token: 'getToken'
     })
   },
-  onGetToken(){
-    wx.request({
-      url: 'http://127.0.0.1:8000/wx/getAccess',
-      data: {}
+  onLogin() {
+    getOpenid().then(res=>{
+      this.setData({
+        openid: res,
+      })
+      console.log(res)
     })
-  }
+  },
+  onGetToken() {}
 })
